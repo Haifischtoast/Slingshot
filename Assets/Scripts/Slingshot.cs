75def355000d235cc6e6764b6388603d8ab3b2b5
Lipo <tmlpbartleby@web.de>
2015-04-27 11:48:58 +0200

    slingshot works

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject launchPoint;
+
+	void Awake() {
+		Transform launchPointTrans = transform.Find("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+	}
+
+	void OnMouseEnter() {
+		print ("jo");
+		launchPoint.SetActive(true);
+	}
+
+	void OnMouseExit() {
+		print ("no");
+		launchPoint.SetActive(false);
+	}
+}
