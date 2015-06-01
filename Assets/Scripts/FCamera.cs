0b5c4bfd6253d81b89cf2f1973833902d2f08bbd
Lipo <tmlpbartleby@web.de>
2015-06-01 10:56:18 +0200

    wolken und shit

---
+++ Assets/Scripts/FCamera.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FCamera : MonoBehaviour {
+
+	public static FCamera s; //singleton instance of this case
+	public GameObject poi;
+	private float camZ;
+
+
+
+	public float ease = 0.05f;
+
+	Vector2 minXY;
+
+	void Awake() {
+
+		s = this; 
+		camZ = transform.position.z;
+
+	}
+
+	void Update () {
+
+
+
+		//check if poi is empty
+		if (poi == null){
+
+			return;
+			}
+
+		Vector3 destination = Vector3.Lerp (transform.position, poi.transform.position , ease);
+
+		destination.x = Mathf.Max (minXY.x, destination.x ); 
+		destination.y = Mathf.Max (minXY.y, destination.y ); 
+
+		destination = Vector3.Lerp(transform.position , destination , ease);
+
+		destination.z = camZ;
+
+		transform.position = destination;
+
+		this.GetComponent<Camera>().orthographicSize = 10 + destination.y;
+	}
+
+
+}
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,13 +2,24 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+	// public
 
-	public GameObject launchPoint;
+	public GameObject prefabProjectile;
+	public float velocityMult = 10.0f;
+
+
+	//privat
+
+	private GameObject launchPoint;
+	private bool aimingMode;
+	private Vector3 launchPos;
+	private GameObject projectile;
 
 	void Awake() {
 		Transform launchPointTrans = transform.Find("LaunchPoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive(false);
+		launchPos = launchPoint.transform.position;
 	}
 
 	void OnMouseEnter() {
@@ -20,4 +31,53 @@ public class Slingshot : MonoBehaviour {
 		print ("no");
 		launchPoint.SetActive(false);
 	}
+
+	void OnMouseDown() {
+		// Set the game to aiming mode
+		aimingMode = true;
+
+		//instanciate a projectile
+		projectile = Instantiate(prefabProjectile) as GameObject;
+
+
+		//Position the projectile at the launchPoint
+		projectile.transform.position = launchPos;
+
+		// disusable kinematic physics for now
+
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
+
+	
+	}
+
+	void Update() {
+		// Check aimingmode
+		if (!aimingMode)return;
+
+		// MousePos in 3dspace 
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+		// delta between mouse & launch pos.
+		Vector3 mouseDelta = mousePos3D - launchPos;
+		// set projectile to new pos und beschränk it auf shperecollider
+		float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+		mouseDelta = Vector3.ClampMagnitude(mouseDelta, maxMagnitude);
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+		// check mouse button released
+		// fire
+		if (Input.GetMouseButtonUp(0)){
+			aimingMode = false;
+			projectile.GetComponent<Rigidbody>().isKinematic = false;
+			projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+			FCamera.s.poi = projectile;
+		}
+
+
+
+	}
+
+
 }
